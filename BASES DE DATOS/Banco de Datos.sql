574f794343795de98c9dc1b22f0c38b1e0e551e4
Bryan31542 <00063820@uca.edu.sv>
2021-06-22 16:41:15 -0600

    Updates

---
+++ BASES DE DATOS/Banco de Datos.sql
@@ -0,0 +1,123 @@
+DROP DATABASE PROJECT;
+CREATE DATABASE PROJECT;
+USE PROJECT;
+SET LANGUAGE us_english;
+
+-- Tables section
+CREATE TABLE EMPLOYEE_TYPE(
+	id_type INT PRIMARY KEY IDENTITY,
+	typename VARCHAR(50)
+);
+
+CREATE TABLE EMPLOYEE(
+	id_employee INT PRIMARY KEY IDENTITY,
+	employee_name VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	employee_address VARCHAR(50) NOT NULL,
+	employee_username VARCHAR(50),
+	employee_password VARCHAR(50),
+	id_type INT NOT NULL,
+	id_booth INT,
+	id_vaccination_place INT NOT NULL
+);
+
+CREATE TABLE BOOTH(
+	id_booth INT PRIMARY KEY IDENTITY,
+	booth_address VARCHAR(50) NOT NULL,
+	telephone INT NOT NULL,
+	email VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE MANAGEMENT(
+	id_management INT PRIMARY KEY IDENTITY,
+	id_employee INT NOT NULL,
+	id_booth INT NOT NULL,
+	date_time_login DATETIME,
+	date_time_logout DATETIME
+);
+
+CREATE TABLE APPOINTMENT(
+	id_appointment INT PRIMARY KEY IDENTITY,
+	date_time_appointment_1 DATETIME,
+	date_time_appointment_2 DATETIME,
+	id_booth INT NOT NULL,
+	id_vaccination_place INT NOT NULL,
+	id_citizen INT NOT NULL
+);
+
+CREATE TABLE VACCINATION_PLACE(
+	id_vaccination_place INT PRIMARY KEY IDENTITY,
+	vaccination_place VARCHAR(50) NOT NULL,
+	vaccination_place_address VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE CITIZEN(
+	id_citizen INT PRIMARY KEY IDENTITY,
+	dui INT NOT NULL,
+	citizen_name VARCHAR(50) NOT NULL,
+	citizen_address VARCHAR(50) NOT NULL,
+	telephone INT NOT NULL,
+	email VARCHAR(50),
+	id_institution INT NOT NULL,
+	disability BIT DEFAULT(0)
+);
+
+CREATE TABLE INSTITUTION(
+	id_institution INT PRIMARY KEY IDENTITY,
+	institution VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE VACCINATION(
+	id_vaccination INT PRIMARY KEY IDENTITY,
+	date_time_wait_1 DATETIME,
+	date_time_vaccine_1 DATETIME,
+	date_time_wait_2 DATETIME,
+	date_time_vaccine_2 DATETIME,
+	id_citizen INT NOT NULL,
+	id_vaccination_place INT NOT NULL
+);
+
+CREATE TABLE SIDE_EFFECT(
+	id_side_effect INT PRIMARY KEY IDENTITY,
+	side_effect VARCHAR(50) NOT NULL,
+	side_effect_description VARCHAR(50)
+);
+
+CREATE TABLE VACCINATIONxSIDE_EFFECT(
+	id_side_effect INT NOT NULL,
+	id_vaccination INT NOT NULL,
+	date_time_showup DATETIME
+);
+
+CREATE TABLE CHRONIC_DISEASE(
+	id_chronic_disease INT PRIMARY KEY IDENTITY,
+	chronic_disease VARCHAR(50) NOT NULL,
+	chronic_disease_description VARCHAR(50)
+);
+
+CREATE TABLE CITIZENxCHRONIC_DISEASE(
+	id_chronic_disease INT NOT NULL,
+	id_citizen INT NOT NULL
+);
+
+-- PK/FK section
+-- PK DEFINITION
+ALTER TABLE VACCINATIONxSIDE_EFFECT ADD PRIMARY KEY (id_side_effect, id_vaccination);
+ALTER TABLE CITIZENxCHRONIC_DISEASE ADD PRIMARY KEY (id_chronic_disease, id_citizen);
+
+-- FK Definition
+ALTER TABLE EMPLOYEE ADD FOREIGN KEY (id_type) REFERENCES EMPLOYEE_TYPE (id_type);
+ALTER TABLE EMPLOYEE ADD FOREIGN KEY (id_booth) REFERENCES BOOTH (id_booth);
+ALTER TABLE EMPLOYEE ADD FOREIGN KEY (id_vaccination_place) REFERENCES VACCINATION_PLACE (id_vaccination_place);
+ALTER TABLE MANAGEMENT ADD FOREIGN KEY (id_booth) REFERENCES BOOTH (id_booth);
+ALTER TABLE MANAGEMENT ADD FOREIGN KEY (id_employee) REFERENCES EMPLOYEE (id_employee);
+ALTER TABLE APPOINTMENT ADD FOREIGN KEY (id_booth) REFERENCES BOOTH (id_booth);
+ALTER TABLE APPOINTMENT ADD FOREIGN KEY (id_vaccination_place) REFERENCES VACCINATION_PLACE (id_vaccination_place);
+ALTER TABLE APPOINTMENT ADD FOREIGN KEY (id_citizen) REFERENCES CITIZEN (id_citizen);
+ALTER TABLE CITIZEN ADD FOREIGN KEY (id_institution) REFERENCES INSTITUTION (id_institution);
+ALTER TABLE VACCINATION ADD FOREIGN KEY (id_citizen) REFERENCES CITIZEN (id_citizen);
+ALTER TABLE VACCINATION ADD FOREIGN KEY (id_vaccination_place) REFERENCES VACCINATION_PLACE (id_vaccination_place);
+ALTER TABLE CITIZENxCHRONIC_DISEASE ADD FOREIGN KEY (id_citizen) REFERENCES CITIZEN (id_citizen);
+ALTER TABLE CITIZENxCHRONIC_DISEASE ADD FOREIGN KEY (id_chronic_disease) REFERENCES CHRONIC_DISEASE (id_chronic_disease);
+ALTER TABLE VACCINATIONxSIDE_EFFECT ADD FOREIGN KEY (id_side_effect) REFERENCES SIDE_EFFECT (id_side_effect);
+ALTER TABLE VACCINATIONxSIDE_EFFECT ADD FOREIGN KEY (id_vaccination) REFERENCES VACCINATION (id_vaccination);
